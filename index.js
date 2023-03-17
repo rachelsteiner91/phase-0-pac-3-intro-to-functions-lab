8865b4cf888c7bb741847d19eef5d8911c2781b9
Rachel Steiner <steinerr310@gmail.com>
2023-03-17 15:34:54 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string) {return string.toUpperCase()}
+"Hello!".toUpperCase()
+function whisper(string) {return string.toLowerCase()}
+"hello" .toLowerCase()
+function logShout(string) {console.log("HELLO")} logShout()
+"hello" .toUpperCase()
+function logWhisper(string) {console.log("hello")} logWhisper()
+"hello" .toLowerCase()
+function sayHiToHeadphonedRoommate(string) {if (string.toLowerCase() === string) {return "I can't hear you!"} if (string.toUpperCase() === string){return "YES INDEED!"} if (string === "Let's have dinner together!") return "I would love to!"}
+
+
